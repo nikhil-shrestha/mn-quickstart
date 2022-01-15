1bc15eca7d2fe615ea05ac97afca4f0f66a2ba52
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2022-01-15 20:50:22 +0545

    first commit

---
+++ src/main/java/com/illusionist/hello/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.illusionist.hello;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloWorldController {
+
+  @Get(produces = MediaType.TEXT_PLAIN)
+  public String helloWorld(){
+    return "hello World!!";
+  }
+
+
+}
