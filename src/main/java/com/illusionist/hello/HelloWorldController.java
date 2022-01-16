aa70027455c0fce76adeda6b680dcf7a476ee43d
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2022-01-16 11:27:45 +0545

    added logging config

--- src/main/java/com/illusionist/hello/HelloWorldController.java
+++ src/main/java/com/illusionist/hello/HelloWorldController.java
@@ -3,9 +3,13 @@ package com.illusionist.hello;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/hello")
 public class HelloWorldController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
 
   private final MyService service;
 
@@ -15,6 +19,7 @@ public class HelloWorldController {
 
   @Get(produces = MediaType.TEXT_PLAIN)
   public String helloWorld(){
+    LOG.debug("Called the helloWorld API");
     return service.helloFromService();
   }
 
